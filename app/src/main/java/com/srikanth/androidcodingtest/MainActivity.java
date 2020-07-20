aeea194312cf517c4ae38af7d16ab6db0049e6ab
Sri786 <srikanth.gutti@outlook.com>
2020-07-20 16:28:05 +0530

    Implemented recyclerview with mock data

--- app/src/main/java/com/srikanth/androidcodingtest/MainActivity.java
+++ app/src/main/java/com/srikanth/androidcodingtest/MainActivity.java
@@ -4,17 +4,34 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.srikanth.androidcodingtest.adapter.Adapter;
+import com.srikanth.androidcodingtest.model.Row;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-
+    private ArrayList<Row> rowData = new ArrayList<>();
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView= findViewById(R.id.recyclerview);
+        recyclerView = findViewById(R.id.recyclerview);
+
+        rowData.add(new Row("Sri", "Android app developer jfhsfsflk sdkhjfjhsdjkfj kjhsdfjhhjsdf", 0));
+        rowData.add(new Row("Sri", "Android app developer jfhsfsflk sdkhjfjhsdjkfj kjhsdfjhhjsdf jfhsfsflk sdkhjfjhsdjkfj kjhsdfjhhjsdf", 1));
+        rowData.add(new Row("Sri", null, 2));
+        rowData.add(new Row(null, "Android app developer", 3));
+        rowData.add(new Row("Sri", "Android app developer", 4));
+
+        Adapter adapter = new Adapter(rowData);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
     }
 }
--- app/src/main/java/com/srikanth/androidcodingtest/adapter/Adapter.java
+++ app/src/main/java/com/srikanth/androidcodingtest/adapter/Adapter.java
@@ -1,32 +1,59 @@
 package com.srikanth.androidcodingtest.adapter;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.srikanth.androidcodingtest.R;
+import com.srikanth.androidcodingtest.model.Row;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Adapter extends RecyclerView.Adapter<Adapter.viewHolder> {
+
+    private ArrayList<Row> rowData;
+
+    public Adapter(ArrayList<Row> rowData) {
+        this.rowData = rowData;
+    }
+
     @NonNull
     @Override
     public Adapter.viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
+        return new viewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull Adapter.viewHolder holder, int position) {
-
+        Row row = rowData.get(position);
+        holder.tvTitle.setText(row.getTitle());
+        holder.tvDesc.setText(row.getDescription());
+        //TODO need to work here
+        holder.ivImage.setImageResource(R.drawable.ic_launcher_foreground);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return rowData.size();
     }
 
     public class viewHolder extends RecyclerView.ViewHolder {
 
+        ImageView ivImage;
+        TextView tvTitle, tvDesc;
+
         public viewHolder(@NonNull View itemView) {
             super(itemView);
+            ivImage = itemView.findViewById(R.id.iv_image);
+            tvTitle = itemView.findViewById(R.id.tv_title);
+            tvDesc = itemView.findViewById(R.id.tv_description);
         }
     }
 }
